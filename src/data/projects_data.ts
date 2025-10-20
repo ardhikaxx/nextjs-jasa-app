2e231527f0294c1851e31aaff56b38608147e9ee
ardhikaxx <ardhikayanuar58@gmail.com>
2025-10-20 20:18:11 +0700

    fix(checkout) perbaikan pada halaman projek + update projek

--- src/data/projects_data.ts
+++ src/data/projects_data.ts
@@ -12,6 +12,9 @@ import Projek11 from '@/assets/projects/11.png';
 import Projek12 from '@/assets/projects/12.jpg';
 import Projek13 from '@/assets/projects/13.png';
 import Projek14 from '@/assets/projects/14.png';
+import Projek15 from '@/assets/projects/15.png';
+import Projek16 from '@/assets/projects/16.png';
+import Projek17 from '@/assets/projects/17.png';
 
 export interface Project {
     id: string;
@@ -164,5 +167,35 @@ export const projectsData: Project[] = [
         link: "https://batik-sesa-bojonegoro.vercel.app/",
         image: Projek14.src,
         category: "Mobile Development"
+    },
+    {
+        id: "15",
+        name: "Website JasaMarga Operasional - Indonesia Highway Corp",
+        description: "Website JasaMarga Operasional – Indonesia Highway Corp adalah one-stop portal untuk memantau status ruas, letak perbaikan jalan tol, fitur laporan berupa file excel dengan perbulan atau pertahun, semua dalam satu halaman cepat, ringan, dan mobile-friendly.",
+        technologies: ["Laravel", "Php", "Html", "Css", "Javascript", "Bootstrap"],
+        year: 2025,
+        link: "https://github.com/ardhikaxx/jasamarga-web",
+        image: Projek15.src,
+        category: "Web Development"
+    },
+    {
+        id: "16",
+        name: "Design UI UX - Aplikasi SIMPATI (Sistem Informasi Pasien TBC)",
+        description: "Desain ulang (redesign) dan pembuatan prototype aplikasi mobile dilakukan untuk memperbaiki struktur alur pengguna sehingga pengalaman penggunaan menjadi lebih intuitif dan terarah",
+        technologies: ["Figma"],
+        year: 2025,
+        link: "https://github.com/ardhikaxx/jasamarga-web",
+        image: Projek16.src,
+        category: "Design UI UX"
+    },
+    {
+        id: "17",
+        name: "Design UI UX - Sistem Informasi STIE BIMA",
+        description: "Desain UI/UX Sistem Informasi Sekolah Tinggi Ilmu Ekonomi (STIE) Bima. Sistem ini terdiri dari lima website operasional, yaitu: Sistem Informasi Akademik, Elektronik Office (E-Office), Sistem Registrasi Mahasiswa, Elektronik Sign (E-Sign), dan Elektronik Pay (E-Pay).",
+        technologies: ["Figma"],
+        year: 2025,
+        link: "https://github.com/ardhikaxx/jasamarga-web",
+        image: Projek17.src,
+        category: "Design UI UX"
     },
 ];
